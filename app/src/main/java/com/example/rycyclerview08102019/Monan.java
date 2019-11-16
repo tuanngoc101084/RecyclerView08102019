ff5fd59763aa399536eb5bf5fc9821c4b9d16899
NGUYEN TUAN NGOC <45462425+tuanngoc101084@users.noreply.github.com>
2019-11-16 20:50:29 +0900

    commit first

---
+++ app/src/main/java/com/example/rycyclerview08102019/Monan.java
@@ -0,0 +1,42 @@
+package com.example.rycyclerview08102019;
+
+public class Monan {
+    private String ten;
+    private String mota;
+    private int hinhanh;
+
+    public Monan() {
+    }
+
+    public Monan(String ten, String mota, int hinhanh) {
+        this.ten = ten;
+        this.mota = mota;
+        this.hinhanh = hinhanh;
+    }
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public int getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(int hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+
+
+}
+
